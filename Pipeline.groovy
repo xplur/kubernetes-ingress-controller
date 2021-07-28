183f70c330b018f58929ee8fbbeb3107029e8a48
ccfishk <jimin.susan.hu@gmail.com>
2021-07-28 15:49:06 -0700

    Fix pipeline failure

--- Pipeline.groovy
+++ Pipeline.groovy
@@ -9,24 +9,25 @@ nodesByLabel('master').each {
         sh('whoami')
         sh('lsblk')
 
+        sh('echo "sync code from test branch ..."')
         dir('/home/centos/go/src/github.com/kong/kubernetes-ingress-controller') {
             checkout scm
         }
 
         sh('sudo chmod -R 777 /home/centos/go/src/github.com/kong/kubernetes-ingress-controller')
-        sh(' creating test cluster ...')
+        sh('echo "creating test cluster ..." ')
         sh('cd /home/centos/go/src/github.com/kong/kubernetes-ingress-controller/railgun && make test.integration.cluster')
 
-        sh('building docker iamge if not yet.')
+        sh('echo "building docker iamge if not yet."')
         sh('docker build -t 477502 -f Dockerfile.Test .')
 
-        sh('compose docker container.')
+        sh('echo "compose docker container."')
         sh('docker run --name execution -t -d -u 997:994 --volume-driver=nfs --network=host --privileged -v /home/centos/go:/home/centos/go -v /var/run/docker.sock:/var/run/docker.sock 477502:latest')
 
-        sh('deploy controller into namesapce kong.')
+        sh('echo "deploy controller into kong namespace."')
         sh('''docker exec -i execution /bin/bash -c "cd /home/centos/go/src/github.com/kong/kubernetes-ingress-controller && kubectl apply -f deploy/single-v2/all-in-one-dbless.yaml''')
         
-        sh('kick off test cases.')
+        sh('echo "kick off test cases."')
         sh('''docker exec -i execution /bin/bash -c "cd /home/centos/go/src/github.com/kong/kubernetes-ingress-controller/railgun && 
         GO111MODULE=on TEST_DATABASE_MODE="off" GOFLAGS="-tags=performance_tests" go test -run "TestIngressPerformance" ./test/performance/ -v''')        
 
