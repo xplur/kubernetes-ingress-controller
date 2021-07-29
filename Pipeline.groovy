3a69584343eadac01aa23f559b476b8e196ede2d
ccfishk <jimin.susan.hu@gmail.com>
2021-07-29 09:06:43 -0700

    clean host configuration and isolated reproducible

--- Pipeline.groovy
+++ Pipeline.groovy
@@ -14,12 +14,12 @@ nodesByLabel('master').each {
             checkout scm
         }
 
-        sh('export GOPATH=/home/centos/go && export GOROOT=/usr/local/go && export GOBIN=/home/centos/go/bin && export PATH=$PATH:$GOROOT/bin:$GOBIN:$GOPATH:/usr/local/bin/')
+        //sh('export GOPATH=/home/centos/go && export GOROOT=/usr/local/go && export GOBIN=/home/centos/go/bin && export PATH=$PATH:$GOROOT/bin:$GOBIN:$GOPATH:/usr/local/bin/')
         
         sh('sudo chmod -R 777 /home/centos/go/src/github.com/kong/kubernetes-ingress-controller')
         
         sh('echo "creating test cluster ..." ')
-        sh('export GOPATH=/home/centos/go && export GOROOT="/usr/local/go" && export GOBIN="/home/centos/go/bin" && export PATH=$PATH:$GOROOT/bin:$GOBIN:$GOPATH:/usr/local/bin/ && cd /home/centos/go/src/github.com/kong/kubernetes-ingress-controller/railgun && make test.integration.cluster')
+        sh('cd /home/centos/go/src/github.com/kong/kubernetes-ingress-controller/railgun && make test.integration.cluster')
 
         sh('echo "building docker iamge if not yet."')
         sh('docker build -t 477502 -f Dockerfile.Test .')
@@ -31,7 +31,7 @@ nodesByLabel('master').each {
         sh('''docker exec -i jenkins /bin/bash -c "cd /home/centos/go/src/github.com/kong/kubernetes-ingress-controller && kubectl apply -f deploy/single-v2/all-in-one-dbless.yaml"''')
         
         sh('echo "kick off test cases."')
-        sh('''docker exec -i jenkins /bin/bash -c "export GOPATH=/home/centos/go && export GOROOT="/usr/local/go" && export GOBIN="/home/centos/go/bin" && export PATH=$PATH:$GOROOT/bin:$GOBIN:$GOPATH:/usr/local/bin/ && GO111MODULE=on TEST_DATABASE_MODE="off" GOFLAGS="-tags=performance_tests" go test -run "TestIngressPerformance" ./test/performance/ -v"''')        
+        sh('''docker exec -i jenkins /bin/bash -c "GO111MODULE=on TEST_DATABASE_MODE="off" GOFLAGS="-tags=performance_tests" go test -run "TestIngressPerformance" ./test/performance/ -v"''')        
 
         }
     }
