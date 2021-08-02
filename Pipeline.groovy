d227af7748464f650bd50654dda69202c5f5ec3c
ccfishk <jimin.susan.hu@gmail.com>
2021-08-01 17:29:53 -0700

    Fix CI

--- Pipeline.groovy
+++ Pipeline.groovy
@@ -19,14 +19,13 @@ nodesByLabel('master').each {
         //sh('sudo chmod -R 777 /home/centos/go/src/github.com/kong/kubernetes-ingress-controller')
         
         sh('echo "creating test cluster ..." ')
-        //sh('whoami')
-        //sh('export GOPATH=/home/centos/go && export GOROOT=/usr/local/go && export GOBIN=/home/centos/go/bin && export PATH=$PATH:$GOROOT/bin:$GOBIN:$GOPATH:/usr/local/bin/ && cd /home/centos/go/src/github.com/kong/kubernetes-ingress-controller && GOFLAGS="-tags=integration_tests" go test -race -v -run "SuiteOnly" ./test/integration/')
+        sh('export GOPATH=/home/centos/go && export GOROOT=/usr/local/go && export GOBIN=/home/centos/go/bin && export PATH=$PATH:$GOROOT/bin:$GOBIN:$GOPATH:/usr/local/bin/ && cd /home/centos/go/src/github.com/kong/kubernetes-ingress-controller && GOFLAGS="-tags=integration_tests" go test -race -v -run "SuiteOnly" ./test/integration/')
 
         sh('echo "building docker iamge if not yet."')
         sh('cd /home/centos/go/src/github.com/kong/kubernetes-ingress-controller && docker build -t 477502 -f Dockerfile.Test .')
 
         sh('echo "compose docker container."')
-        sh('docker run --name jenkins -t -d --user root --volume-driver=nfs --network=host --privileged -v /bin:/bin -v /etc/sysconfig/docker:/etc/sysconfig/docker -v /root:/root -v /usr/local/bin:/usr/local/bin -v /home/centos:/home/centos -v /var/run/docker.sock:/var/run/docker.sock 477502:latest')
+        sh('docker stop jenkins && docker rm jenkins && docker run --name jenkins -t -d --user root --volume-driver=nfs --network=host --privileged -v /bin:/bin -v /etc/sysconfig/docker:/etc/sysconfig/docker -v /root:/root -v /usr/local/bin:/usr/local/bin -v /home/centos:/home/centos -v /var/run/docker.sock:/var/run/docker.sock 477502:latest')
 
         sh('echo "deploy controller into kong namespace."')
         sh('''docker exec -i jenkins /bin/bash -c "kubectl apply -f /home/centos/deployment/all-in-one-postgres.yaml"''')
